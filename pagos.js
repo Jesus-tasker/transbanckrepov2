2450464f5e8953c832337281a156bd81316aa734
Jesus-tasker <jesusmontoya105@gmail.com>
2024-07-27 02:12:39 -0400

    nuevos dominios

--- pagos.js
+++ pagos.js
@@ -35,7 +35,7 @@ const cors=require('cors');
 
 //probamos especificas
 const corsOptions = {
-  origin: ['http://localhost:51118', 'https://newlove.cl','https://newlove.cl/#/','https://casinoscripto.netlify.app/#/','https://portalcapital.netlify.app/#/','https://casinoscripto.co/#/','https://portalcapital.cl/#/'], // Lista de URLs permitidas
+  origin: ['http://localhost:51118','eloquent-halva-f6f5f1.netlify.app','newlove.cl', 'https://newlove.cl','https://newlove.cl/#/','https://casinoscripto.netlify.app/#/','https://portalcapital.netlify.app/#/','https://casinoscripto.co/#/','https://portalcapital.cl/#/'], // Lista de URLs permitidas
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
   credentials: true, // Habilita el intercambio de cookies o credenciales
   optionsSuccessStatus: 204, // Respuesta exitosa para las solicitudes OPTIONS
